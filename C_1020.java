b4625e2bd71fcc2f4addda67ff6e594dd0e598ce
Justin <sinkashot@gmail.com>
2022-01-04 10:40:17 +0900

    codeup 1020

---
+++ C_1020.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class C_1020 {
+    public static void main(String[] args) {
+        String privacyNumber;
+        String[] splitNumber;
+
+        Scanner sc = new Scanner(System.in);
+        privacyNumber = sc.nextLine();
+        sc.close();
+
+        splitNumber = privacyNumber.split("\\-");
+        System.out.println(splitNumber[0]+splitNumber[1]);
+    }
+}
